3369c80c90b29b89e37f8756e05bf59abffcbb51
Joel McCune <knu2xs@gmail.com>
2017-01-24 16:43:54 -0800

    script working

--- index.js
+++ index.js
@@ -1,4 +1,57 @@
-//
+"use strict";
+/* global require */
 
-// packages
-const speecTest = require('speedtest.net');
+// hard variables to ensure always the same place
+const dynamoTableName = "internet-speed",
+    logDirectory = "/var/log",
+    logFile = "speedtest" + new Date().getTime() + ".log";
+
+// package imports
+const speedTest = require('speedtest.net'),
+    AWS = require('aws-sdk'),
+    uuid = require('uuid/v4');
+
+// import local location configuration
+const localConfig = require('./resources/local_config');
+
+// set Amazon configuration options
+AWS.config.loadFromPath('./resources/aws_config.json');
+
+// function to put results into Amazon DynamoDB
+const putToDynamo = function(item){
+
+  // create the document client object for working with AWS
+  const docClient = new AWS.DynamoDB.DocumentClient();
+
+  // create the put parameters
+  const parameters = {
+    TableName: dynamoTableName,
+    Item: item
+  };
+
+  // put the data into the table
+  docClient.put(parameters, function(error, data){
+    if(error) console.log(error);
+    else console.log(data);
+  });
+  
+};  // close putToDynamo
+
+
+// run the SpeedTest
+speedTest.runTest(function (speedTestResponse) {
+
+  // clean up and add all values to JSON item object
+  const item = {
+    uuid: uuid(),
+    timestamp: speedTestResponse.timestamp,
+    physicalAddress: localConfig.physicalAddress.toUpperCase(),
+    serviceProvider: localConfig.serviceProvider.toUpperCase(),
+    speedDownload: parseFloat(speedTestResponse.download),
+    speedUpload: parseFloat(speedTestResponse.upload)
+  };
+  
+  // put the results into Dynamo
+  putToDynamo(item);
+
+});  // close runTest
